e1f2f281527360165723e2301fb9630542224f16
xarrijorge <kodinglife@gmail.com>
2022-08-09 20:53:43 +0000

    Styled rejection view

--- views/rejection.js
+++ views/rejection.js
@@ -4,8 +4,316 @@ module.exports = function (user, req) {
         to: [user['Employee Email Address']],
         bcc: 'request-tracker@easysolar.org',
         from: 'techadmin@easysolar.org', // Use the email address or domain you verified above
-        subject: `Rejected Request from ${user['Full Name']}`,
+        subject: 'Rejected Petty Cash Request',
         text: `Your Request Was Not Approved of the following reason: ${reason}.`,
-        html: `<h3><a href="${req.body.FILE}">THIS Request</a> Was Not Approved of the following reason: ${reason}.</h3>`,
+        html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+  <style type="text/css">
+    body,
+    p,
+    div {
+      font-family: inherit;
+      font-size: 14px;
+    }
+
+    body {
+      color: #000000;
+    }
+
+    body a {
+      color: #1188E6;
+      text-decoration: none;
+    }
+
+    p {
+      margin: 0;
+      padding: 0;
+    }
+
+    table.wrapper {
+      width: 100% !important;
+      table-layout: fixed;
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-size-adjust: 100%;
+      -moz-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+
+    img.max-width {
+      max-width: 100% !important;
+    }
+
+    .column.of-2 {
+      width: 50%;
+    }
+
+    .column.of-3 {
+      width: 33.333%;
+    }
+
+    .column.of-4 {
+      width: 25%;
+    }
+
+    ul ul ul ul {
+      list-style-type: disc !important;
+    }
+
+    ol ol {
+      list-style-type: lower-roman !important;
+    }
+
+    ol ol ol {
+      list-style-type: lower-latin !important;
+    }
+
+    ol ol ol ol {
+      list-style-type: decimal !important;
+    }
+
+    @media screen and (max-width:480px) {
+
+      .preheader .rightColumnContent,
+      .footer .rightColumnContent {
+        text-align: left !important;
+      }
+
+      .preheader .rightColumnContent div,
+      .preheader .rightColumnContent span,
+      .footer .rightColumnContent div,
+      .footer .rightColumnContent span {
+        text-align: left !important;
+      }
+
+      .preheader .rightColumnContent,
+      .preheader .leftColumnContent {
+        font-size: 80% !important;
+        padding: 5px 0;
+      }
+
+      table.wrapper-mobile {
+        width: 100% !important;
+        table-layout: fixed;
+      }
+
+      img.max-width {
+        height: auto !important;
+        max-width: 100% !important;
+      }
+
+      a.bulletproof-button {
+        display: block !important;
+        width: auto !important;
+        font-size: 80%;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+      }
+
+      .columns {
+        width: 100% !important;
+      }
+
+      .column {
+        display: block !important;
+        width: 100% !important;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+        margin-left: 0 !important;
+        margin-right: 0 !important;
+      }
+
+      .social-icon-column {
+        display: inline-block !important;
+      }
+    }
+  </style>
+  <style>
+    @media screen and (max-width:480px) {
+      table {
+        width: 480px !important;
+      }
+    }
+  </style>
+  <!--user entered Head Start-->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300&display=swap" rel="stylesheet">
+  <style>
+    body {
+      font-family: 'Lato', sans-serif;
+    }
+  </style>
+  <!--End Head user entered-->
+</head>
+
+<body>
+  <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:inherit; color:#000000; background-color:#f3f3f3;">
+    <div class="webkit">
+      <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#f3f3f3">
+        <tr>
+          <td valign="top" bgcolor="#f3f3f3" width="100%">
+            <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+              <tr>
+                <td width="100%">
+                  <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                    <tr>
+                      <td>
+                        <!--[if mso]>
+    <center>
+    <table><tr><td width="600">
+  <![endif]-->
+                        <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                          <tr>
+                            <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left">
+                              <table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+                                <tr>
+                                  <td role="module-content">
+                                    <p></p>
+                                  </td>
+                                </tr>
+                              </table>
+                              <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" role="module" data-type="columns" style="padding:30px 0px 30px 0px;" bgcolor="#f2eefb" data-distribution="1">
+                                <tbody>
+                                  <tr role="module-content">
+                                    <td height="100%" valign="top">
+                                      <table width="600" style="width:600px; border-spacing:0; border-collapse:collapse; margin:0px 0px 0px 0px;" cellpadding="0" cellspacing="0" align="left" border="0" bgcolor="" class="column column-0">
+                                        <tbody>
+                                          <tr>
+                                            <td style="padding:0px;margin:0px;border-spacing:0;">
+                                              <table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="79178f70-3054-4e9f-9b29-edfe3988719e">
+                                                <tbody>
+                                                  <tr>
+                                                    <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="center">
+                                                      <img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px;" width="100" alt="" data-proportionally-constrained="true" data-responsive="false" src="http://cdn.mcauto-images-production.sendgrid.net/de46cb2b5c737d52/abb583a4-6776-4bca-bdd3-e0afee2c194f/136x61.png" height="61">
+                                                    </td>
+                                                  </tr>
+                                                </tbody>
+                                              </table>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="ef0f9e06-1b02-4b22-b5e8-dc8f6bb9b3b1" data-mc-module-version="2019-10-22">
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:50px 20px 10px 20px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content">
+                                      <div>
+                                        <div style="font-family: inherit; text-align: center"><span style="font-size: 28px; font-family: inherit">Request Rejection</span></div>
+                                        <div></div>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="ef0f9e06-1b02-4b22-b5e8-dc8f6bb9b3b1.1.1" data-mc-module-version="2019-10-22">
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:20px 20px 10px 20px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content">
+                                      <div>
+                                        <div style="font-family: inherit; text-align: center">Hello ${user['Full Name']}</div>
+                                        <div style="font-family: inherit; text-align: center"><br></div>
+                                        <div style="font-family: inherit; text-align: center"><h3><a href="${req.body.FILE}">THIS Request</a> Was Not Approved of the following reason:</h3>
+                                          <ul>
+                                            <li>${reason}</li>
+                                          </ul>
+                                        </div>
+                                        <div></div>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="8395333d-62e9-4e61-957d-72d0eefc1a4f">
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="divider" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="86c0feb7-e890-4382-bb8e-b1910742ba10">
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:0px 30px 0px 40px;" role="module-content" height="100%" valign="top" bgcolor="">
+                                      <table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" height="1px" style="line-height:1px; font-size:1px;">
+                                        <tbody>
+                                          <tr>
+                                            <td style="padding:0px 0px 1px 0px;" bgcolor="#000000"></td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="10dfe38b-ab1a-4083-80ca-725cb09e3c1c">
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="f612db9d-7563-4153-b3d5-8a0015929def.1" data-mc-module-version="2019-10-22">
+
+                              </table>
+                              <table class="module" role="module" data-type="divider" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="86c0feb7-e890-4382-bb8e-b1910742ba10.1.1">
+                                <tbody>
+
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:18px 30px 18px 40px; line-height:28px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content">
+                                      <div>
+                                        <div style="font-family: inherit; text-align: center"><span style="font-size: 28px">This Request has been Approved!</span></div>
+                                        <div></div>
+                                      </div>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="10dfe38b-ab1a-4083-80ca-725cb09e3c1c.1">
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="0a0f7040-0a2f-4749-8f52-03f4bfb4f161">
+                                <tbody>
+                                  <tr>
+                                    <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </table>
+                        <!--[if mso]>
+                                  </td>
+                                </tr>
+                              </table>
+                            </center>
+                            <![endif]-->
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </center>
+</body>
+</html>`,
     };
 };
